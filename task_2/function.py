dad4dc4be923a56c40d839434d52a12743be45ad
Caleb Espinoza <caleb.espinoza@outlook.com>
2021-03-01 09:11:30 -0400

    Add task 2

---
+++ task_2/function.py
@@ -0,0 +1,13 @@
+def my_print(*parameters):
+    print(parameters)
+
+my_print(1, 2, 3, "Caleb")
+
+
+
+
+
+#def greet (name1, name2):
+#    print ("Hello {} {}".format(name1, name2))
+
+#greet("Caleb", "Espinoza")
---
+++ task_2/my_print.py
@@ -0,0 +1,5 @@
+def my_print(*parameters):
+    temp_list = '-'.join(parameters)
+    print(temp_list)
+
+my_print("1", "2", "3", "Caleb")
