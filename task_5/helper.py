6bb355ea01428288e78eb7a7cf2406178a37c776
Caleb Espinoza <caleb.espinoza@outlook.com>
2021-03-18 03:02:19 -0400

    Add second solution without using len()

--- task_5/helper.py
+++ task_5/helper.py
@@ -3,7 +3,10 @@ class Helper:
     @staticmethod
     def my_lenght(structure):
         if type(structure) == list or type(structure) == dict or type(structure) == str:
-            print(len(structure))
+            i = 0
+            for key in structure:
+                i = i + 1
+            print(i)
         else:
             raise ValueError("You don't entered a String or a Structure")
 
--- task_5/main.py
+++ task_5/main.py
@@ -2,5 +2,5 @@ from helper import Helper
 
 Helper.my_lenght("123")
 Helper.my_lenght([1,2,3])
-Helper.my_lenght({'a':"a"})
+Helper.my_lenght({'a':"a", 'b':123})
 Helper.my_lenght(123)
